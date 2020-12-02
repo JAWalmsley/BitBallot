7e7ec4af8c251333f6cbd1b077a9a31cd573a6d7
Jack Walmsley <jackawalmsley@gmail.com>
2020-12-02 17:49:20 -0500

    Added Template Endpoints

--- BitBallotNode/app.py
+++ BitBallotNode/app.py
@@ -3,9 +3,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/register')
+def register():
+    """ Client-Facing
+
+    Registers a new voter
+    """
+    pass
+
+@app.route('/vote')
+def vote():
+    """ Client-Facing
+
+    Submits a vote for a candidate
+    """
+    pass
+
+@app.route('/sync')
+def sync():
+    """ Internally-Facing
+
+    Recieve new blocks from other nodes
+    """
+    pass
 
 
 if __name__ == '__main__':
