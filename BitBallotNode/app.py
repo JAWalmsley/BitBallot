5d8ed4276055d7dc4a8cff747c8ab4c1dbc3d29c
Jack Walmsley <jackawalmsley@gmail.com>
2020-12-02 18:50:55 -0500

    Added parameters to route templates

--- BitBallotNode/app.py
+++ BitBallotNode/app.py
@@ -1,31 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     """ Client-Facing
 
     Registers a new voter
     """
-    pass
+    user_id = request.form["user_id"]
+    return user_id
 
-@app.route('/vote')
+
+@app.route('/vote', methods=['POST'])
 def vote():
     """ Client-Facing
 
     Submits a vote for a candidate
     """
-    pass
+    # TODO: Verify user id 
+    user_id = request.form["user_id"]
+    choice = request.form["choice"]
+    return jsonify([user_id, choice])
 
-@app.route('/sync')
+
+@app.route('/sync', methods=['POST'])
 def sync():
     """ Internally-Facing
 
     Recieve new blocks from other nodes
     """
-    pass
+    # TODO: Verify sender's key
+    # TODO: Add new block to local blockchain
+
+    new_block = request.form["new_block"]
+    return new_block
 
 
 if __name__ == '__main__':
