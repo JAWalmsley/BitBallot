4033b981922a6bdca9fdb19de173c104e4c6361d
Jack Walmsley <jackawalmsley@gmail.com>
2021-01-20 23:23:25 -0500

    Added QR code generation to card

--- Card Generator/BitBallot Ticket Generator.py
+++ Card Generator/BitBallot Ticket Generator.py
@@ -1,11 +1,14 @@
 # Jack Walmsley 2020-12-26
-BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-import random, pathlib, os
-
+import os
+import pathlib
+import random
+import qrcode
 from PIL import Image, ImageFont, ImageDraw
+BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+font = ImageFont.truetype("C:\\Windows\\Fonts\\micross.ttf", 12)
 
 
-def encode(num, alphabet):
+def encode(num: int, alphabet: str):
     """Encode a positive number into Base X and return the string.
 
     :param num: the number to encode
@@ -22,45 +25,45 @@ def encode(num, alphabet):
     return ''.join(arr)
 
 
-def decode(string, alphabet=BASE62):
-    """Decode a Base X encoded string into the number
+def create_card(id: str):
+    """Creates a card image and saves it to /output/card_[ID].png
 
-    Arguments:
-    - `string`: The encoded string
-    - `alphabet`: The alphabet to use for decoding
+    :param id: id to print on card
     """
-    base = len(alphabet)
-    strlen = len(string)
-    num = 0
-
-    idx = 0
-    for char in string:
-        power = (strlen - (idx + 1))
-        num += alphabet.index(char) * (base ** power)
-        idx += 1
-
-    return num
-
-
-def create_card(id):
-    imgpath = os.path.join(pathlib.Path(__file__).parent.absolute(), "BitBallot Card.png")
+    imgpath = os.path.join(pathlib.Path(
+        __file__).parent.absolute(), "BitBallot Card.png")
     img = Image.open(imgpath)
     draw = ImageDraw.Draw(img)
+    textpos_x, textpos_y = 215, 385
+    text_bord = 4
+    qrpos_x, qrpos_y = 180, 315
+
+    qr = qrcode.QRCode(
+        version=2,
+        error_correction=qrcode.constants.ERROR_CORRECT_Q,
+        box_size=4,
+        border=0,
+    )
+    qr.add_data(id)
+    qr.make(fit=True)
+    code = qr.make_image(fill_color="black", back_color="white")
+
     w, h = draw.textsize(id, font)
-    draw.text((200-w/2, 310-h/2), id, (0, 0, 0), font=font)
-    img.save(os.path.join(pathlib.Path(__file__).parent.absolute(), "output/card_{}.png".format(id)))
+    draw.text((textpos_x-w//2, textpos_y-h//2), id, (0, 0, 0), font=font)
+    draw.rectangle(((textpos_x-w//2 - text_bord, textpos_y-h//2 - text_bord),
+                    (textpos_x+w//2 + text_bord, textpos_y+h//2 + text_bord)), outline="black")
+    img.paste(code, (qrpos_x-code.width//2, qrpos_y-code.height//2))
+    img.save(os.path.join(pathlib.Path(__file__).parent.absolute(),
+                          "output/card_{}.png".format(id)))
     print("INSERT INTO uids VALUES(\"{}\");".format(id))
 
 
-font = ImageFont.truetype("C:\\Windows\\Fonts\\micross.ttf", 12)
-create_card('ffvZYcSaW17GHN0')
-create_card('5KMeZ1Yc8c7sptQ')
-create_card('cBZs0ITJfKvqwEt')
-create_card('null')
+def create_random_cards(num: int, code_length: int = 15):
+    """Generates random cards
 
-
-def random_cards(num):
+    :param num: number of cards to generate
+    :param code_length: length of codes to print on cards
+    """
     for i in range(num):
-        id = encode(random.randint(0, 768909704948766668552634367), BASE62)
+        id = encode(random.randint(0, 62**code_length), BASE62)
         create_card(id)
-
