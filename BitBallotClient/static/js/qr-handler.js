065f1d46fd7a62ed97705254d48234af2998ebe7
Jack Walmsley <jackawalmsley@gmail.com>
2021-01-21 20:49:08 -0500

    QR Scanner does not show if camera not found

--- BitBallotClient/static/js/qr-handler.js
+++ BitBallotClient/static/js/qr-handler.js
@@ -1,10 +1,23 @@
-import QRScanner from '/lib/qr-scanner/qr-scanner.min.js';
-QRScanner.WORKER_PATH = '/lib/qr-scanner/qr-scanner-worker.min.js';
+import QRScanner from "/lib/qr-scanner/qr-scanner.min.js";
+QRScanner.WORKER_PATH = "/lib/qr-scanner/qr-scanner-worker.min.js";
 let videoElem = document.getElementById("scanner");
-const qrScanner = new QRScanner(videoElem, result => postScan(result));
-qrScanner.start();
+let qrScanner = new QRScanner(videoElem, (result) => postScan(result));
+QRScanner.hasCamera().then((hasCamera) => {
+  if (!hasCamera) {
+    cleanUp();
+  } else {
+    qrScanner.start();
+  }
+});
 function postScan(data) {
-    console.log(data);
-    document.getElementById("user_id").value = data;
-    qrScanner.stop();
+  console.log(data);
+  document.getElementById("user_id").value = data;
+  cleanUp();
+}
+
+function cleanUp() {
+  videoElem.style.display = "none";
+  qrScanner.stop();
+  qrScanner.destroy();
+  qrScanner = null;
 }
